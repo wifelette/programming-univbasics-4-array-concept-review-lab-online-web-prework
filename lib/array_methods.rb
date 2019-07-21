429503b05501614d702ec6d7822c8fe264f6b05d
Leah Silber <leahsilber@gmail.com>
2019-07-21 01:10:05 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,9 +3,9 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  array.max
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.min
 end
